964672e3bf25eb2db4856d39e8ef4bd91e1a33e3
Krzysztof Wąsowski <krzysztof.wasowski@intive.com>
2017-07-20 09:57:57 +0200

    code clear

--- lms/static/js/categories/views/tabs.js
+++ lms/static/js/categories/views/tabs.js
@@ -11,9 +11,6 @@
 
             initialize: function() {
                 this.$tabs = this.$el.find('.category-tab');
-                // this.$searchButton = this.$el.find('button');
-                // this.$message = this.$el.find('#discovery-message');
-                // this.$loadingIndicator = this.$el.find('#loading-indicator');
             },
 
             tabClick: function(event){
@@ -30,55 +27,6 @@
             getCategoryTabText: function(index) {
                 return this.$tabs.eq(index).find('a').text();
             }
-
-            // submitForm: function(event) {
-            //     event.preventDefault();
-            //     this.doSearch();
-            // },
-
-            // doSearch: function(term) {
-            //     if (term !== undefined) {
-            //         this.$searchField.val(term);
-            //     }
-            //     else {
-            //         term = this.$searchField.val();
-            //     }
-            //     this.trigger('search', $.trim(term));
-            // },
-
-            // clearSearch: function() {
-            //     this.$searchField.val('');
-            // },
-
-            // showLoadingIndicator: function() {
-            //     this.$loadingIndicator.removeClass('hidden');
-            // },
-
-            // hideLoadingIndicator: function() {
-            //     this.$loadingIndicator.addClass('hidden');
-            // },
-
-            // showFoundMessage: function(count) {
-            //     var msg = ngettext(
-            //     'Viewing %s course',
-            //     'Viewing %s courses',
-            //     count
-            // );
-            //     this.$message.html(interpolate(msg, [count]));
-            // },
-
-            // showNotFoundMessage: function(term) {
-            //     var msg = interpolate(
-            //     gettext('We couldn\'t find any results for "%s".'),
-            //     [_.escape(term)]
-            // );
-            //     this.$message.html(msg);
-            //     this.clearSearch();
-            // },
-
-            // showErrorMessage: function() {
-            //     this.$message.html(gettext('There was an error, try searching again.'));
-            // }
 
         });
     });
