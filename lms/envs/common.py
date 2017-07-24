1c4813dbd90e1dc33622d80f691d9e8fc06e1cae
Karol Pogonowski <karol.pogonowski@intive.com>
2017-07-24 15:36:44 +0200

    FOOTER: fix links

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2261,6 +2261,7 @@ MKTG_URL_LINK_MAP = {
     'HONOR': 'honor',  # If your site does not have an honor code, simply delete this line.
     'PRIVACY': 'privacy',
     'PRESS': 'press',
+    'HELP': 'help',
     'SITEMAP.XML': 'sitemap_xml',
     'CATEGORIES': 'categories',
     'LECTURERS': 'lecturers',
